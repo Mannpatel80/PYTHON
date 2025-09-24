860a494e224cdd056a2065f15c3e708e7c2521c1
Mannpatel80 <mannpatel040412@gmail.com>
2025-09-24 10:53:57 +1000

    project

--- Intrgers.py/porject.py
+++ Intrgers.py/porject.py
@@ -1,22 +1,35 @@
 print("|---------------------------------------------------------|")
-Jimmy=input(" What is your brithday\n",)
-print("|-----------------------------------------------|")
-timmy=input(" What is your brithday\n",)
-print("|-----------------------------------------------|")
-billy=input(" What is your brithday\n",)
-print("|-----------------------------------------------|")
-Frank=input(" What is your brithday\n",)
-print("|-----------------------------------------------|")
-bob=input(" What is your brithday\n",)
-print("|--------------------------------------------------------|")
+Name1=input("What is your name\n")
 
-print("Jimmy, this is you brithday: ",Jimmy)
+Jimmy=input(" What is your brithday\n",)
+
+print( Name1, ", this is you brithday: ",Jimmy)
+
 print("|-----------------------------------------------|")
-print("Timmy, this is you brithday: ",timmy)
+Name2=input("What is your name\n")
+
+timmy=input(" What is your brithday\n",)
+
+print(Name2, ", this is you brithday: ",timmy)
 print("|-----------------------------------------------|")
-print("Billy, this is you brithday: ",billy)
+
+Name3=input("What is your name\n")
+billy=input(" What is your brithday\n",)
+print( Name3, ", this is you brithday: ",billy)
 print("|-----------------------------------------------|")
-print("Frank, this is you brithday: ",Frank)
+
+Name4=input("What is your name\n")
+
+Frank=input(" What is your brithday\n",)
+print( Name4, ", this is you brithday: ",Frank)
 print("|-----------------------------------------------|")
-print("Bob, this is you brithday: ",bob)
+
+
+
+Name5=input("What is your name\n")
+bob=input(" What is your brithday\n",)
+print( Name5, ", this is you brithday: ",bob)
 print("|---------------------------------------------------------|")
+
+
+
