0b9d31ffed6bc02364390230eeea7141e9c91c84
Mannpatel80 <mannpatel040412@gmail.com>
2025-09-29 10:43:50 +1000

    other homework

---
+++ Operaters/1/hi2.py
@@ -0,0 +1,10 @@
+# Program to find the square root of a number
+
+# Take input from the user
+num = float(input("Enter a number: "))
+
+# Calculate square root
+square_root = num ** 0.5
+
+# Display result
+print("The square root of", num, "is", square_root)
