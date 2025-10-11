ce0fa019f4d48d7659aae0f184db59a76ca7f856
Mannpatel80 <mannpatel040412@gmail.com>
2025-10-11 23:13:39 +1000

    u

---
+++ python challages/py/activity2.py
@@ -0,0 +1,11 @@
+print("enter a nuber for numertor")
+numn=int(input())
+
+print("enter the number for denominator")
+numd=int(input())
+
+if numn%numd==0:
+    print("\n" +str(numn)+ " is divisible by " + str(numd))
+
+else:
+     print("\n" +str(numn)+ " is not divisible by 43" + str(numd))
---
+++ u.py
@@ -0,0 +1,11 @@
+num1=1
+num2=2
+num3=3
+
+print("(num1*num3)/num1= ",(num1*num3)/num1)
+
+num1=input("enter first digit")
+num2=input("enter second digit")
+num3=input("enter thrid digit")
+
+print("(num1*num3)/num1= ",(num1*num3)/num1)
